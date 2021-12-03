b721f9721705857840944f39fa62203383094be5
Julián Echeverri <julianecheverri@outlook.com>
2021-12-02 20:57:02 -0500

    Listening on the message bus on console app

--- AirlineSendAgent/App/AppHost.cs
+++ AirlineSendAgent/App/AppHost.cs
@@ -1,4 +1,6 @@
 using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace AirlineSendAgent.App
 {
@@ -6,7 +8,25 @@ namespace AirlineSendAgent.App
     {
         public void Run()
         {
-            Console.WriteLine("Hello World");
+            var connectionFactory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName, exchange: "trigger", routingKey: "");
+
+                var consumer = new EventingBasicConsumer(channel);
+                Console.WriteLine("Listening on the message bus");
+
+                consumer.Received += async (ModuleHandle, ea) =>
+                {
+                    Console.WriteLine("Event is triggered!");
+                };
+
+                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                Console.ReadLine();
+            }
         }
     }
 }
--- AirlineSendAgent/Program.cs
+++ AirlineSendAgent/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using AirlineSendAgent.App;
+﻿using AirlineSendAgent.App;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
